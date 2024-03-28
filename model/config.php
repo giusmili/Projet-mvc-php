bd660189e79e211ca53f812ee6e5a5fd9ca7a2fb
giusmili <giusmili67@gmail.com>
2024-03-28 12:30:37 +0100

    comment php

--- model/config.php
+++ model/config.php
@@ -1,4 +1,5 @@
 <?php
+    # schéma vers la bdd
     try {
         $database = new PDO('mysql:host=localhost;dbname=blogs;charset=utf8', 'root', '');
     } 
@@ -7,7 +8,7 @@
         die('Erreur connexion your bdd : '.$e->getMessage());
     }
 
-    // We retrieve the 5 last blog posts.
+    
     $statement = $database->query(
         "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
 );
